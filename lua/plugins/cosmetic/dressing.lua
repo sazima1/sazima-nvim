43e3e2041ab149a679bf69c221e992a97baefabc
Andy Sazima <sazima1@llnl.gov>
2025-02-08 14:37:19 -0800

    oops

--- lua/plugins/cosmetic/dressing.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"stevearc/dressing.nvim",
-	enabled = false,
-	config = function()
-		require("dressing").setup({})
-	end,
-}
--- lua/plugins/usability/luasnip-snippets.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"mireq/luasnip-snippets",
-	dependencies = { "L3MON4D3/LuaSnip" },
-	opts = {},
-	config = function(_, opts)
-		-- Mandatory setup function
-		require("luasnip_snippets.common.snip_utils").setup(opts)
-	end,
-}
