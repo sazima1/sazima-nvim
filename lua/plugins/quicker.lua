e28994b0916c1a48c3cff89358ca97adc34eb595
Andy Sazima <sazima1@llnl.gov>
2026-02-18 10:16:01 -0800

    quickfix

---
+++ lua/plugins/quicker.lua
@@ -0,0 +1,12 @@
+--[[
+Name: Quicker.nvim
+Language: N/A
+Description: Nicer quickfix functionality and styling
+--]]
+return {
+	"stevearc/quicker.nvim",
+	event = "FileType qf",
+	---@module "quicker"
+	---@type quicker.SetupOptions
+	opts = {},
+}
