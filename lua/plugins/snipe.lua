0596d3df030a04b12945dfcdb47df1f437cafd97
Andy Sazima <sazima1@llnl.gov>
2026-02-18 10:16:15 -0800

    better snipe defaults

--- lua/plugins/snipe.lua
+++ lua/plugins/snipe.lua
@@ -14,17 +14,28 @@ return {
 			},
 			text_align = "file-first",
 			preselect_current = true,
+			-- sort = "last",
 		},
 		hints = {
-			dictionary = "asdfglewcmphio",
+			dictionary = "asdfgwerxcvhmlio",
 		},
-		-- sort = "last",
+		navigate = {
+			cancel_snipe = "q",
+		},
 	},
 	keys = {
 		{
 			"<leader>o",
 			function()
-				require("snipe").open_buffer_menu()
+				local filetype = vim.bo.filetype
+				local filetypes_to_check_for = {
+					"snacks_dashboard",
+				}
+				for _, b in ipairs(filetypes_to_check_for) do
+					if filetype ~= b then
+						require("snipe").open_buffer_menu()
+					end
+				end
 			end,
 			mode = { "n" },
 			noremap = true,
