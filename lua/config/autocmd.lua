888dc2d6150d7deb52150a28a4597a28935e4ddc
Andy Sazima <sazima1@llnl.gov>
2026-02-18 10:12:59 -0800

    add autocommands

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -21,23 +21,17 @@ local group_set_syntax = augroup("group_set_syntax", {})
 autocmd({ "BufNewFile", "BufRead" }, {
 	group = group_set_syntax,
 	pattern = { "*.tg", "*.part", "*.lc", "*.mtrl", "*.key", "*.ig", "trugrdi" },
-	callback = function()
-		set.filetype = "ingrid"
-	end,
+	callback = function() set.filetype = "ingrid" end,
 })
 autocmd({ "BufNewFile", "BufRead" }, {
 	group = group_set_syntax,
 	pattern = { "*.assembly*", "*.subassembly*", "*.diablo*", "*.dbl*" },
-	callback = function()
-		set.filetype = "diablo"
-	end,
+	callback = function() set.filetype = "diablo" end,
 })
 autocmd({ "BufNewFile", "BufRead" }, {
 	group = group_set_syntax,
 	pattern = { "*.bash*" },
-	callback = function()
-		set.filetype = "sh"
-	end,
+	callback = function() set.filetype = "sh" end,
 })
 
 -- Turn on word wrap for word processing file extensions
@@ -45,9 +39,7 @@ local group_word_wrap = augroup("group_word_wrap", {})
 autocmd({ "BufNewFile", "BufRead" }, {
 	group = group_word_wrap,
 	pattern = { "*.tex", "*.latex", "*.md", "*.txt", "*.text" },
-	callback = function()
-		set_local.wrap = true
-	end,
+	callback = function() set_local.wrap = true end,
 })
 
 -- Format on save
@@ -55,9 +47,7 @@ local group_formatting = augroup("group_formatting", {})
 autocmd({ "BufWritePre" }, {
 	group = group_formatting,
 	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+	callback = function(args) require("conform").format({ bufnr = args.buf }) end,
 })
 -- Remove whitespace when files are saved
 -- autocmd({ "BufWritePre" }, {
@@ -89,9 +79,7 @@ autocmd({ "TermOpen" }, {
 local group_linting = augroup("group_linting", { clear = true })
 autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 	group = group_linting,
-	callback = function()
-		require("lint").try_lint()
-	end,
+	callback = function() require("lint").try_lint() end,
 })
 
 -- Disable automatic commenting of next line
@@ -111,8 +99,34 @@ local group_help = augroup("group_help", {})
 autocmd("FileType", {
 	group = group_help,
 	pattern = "help",
-	command = "wincmd L | let w = min([85, &columns / 3]) | execute 'vertical resize ' . w",
+	callback = function() vim.cmd("wincmd L | let w = min([90, &columns / 2]) | execute 'vertical resize ' . w") end,
 })
+-- autocmd("BufEnter", {
+-- 	group = group_help,
+-- 	callback = function(args)
+-- 		local bufnr = args.buf
+-- 		local filetype = vim.bo[bufnr].filetype
+-- 		-- local help_fts = {
+-- 		-- 	"help",
+-- 		-- 	"helpmd",
+-- 		-- }
+-- 		local enew_fts = {
+-- 			"snacks_dashboard",
+-- 		}
+--
+-- 		-- for _, ft in ipairs(enew_fts) do
+-- 		-- 	if filetype == b then
+-- 		-- 	end
+-- 		-- end
+-- 		-- If in dashboard, open an empty buffer to prevent errors
+-- 		for _, b in ipairs(enew_fts) do
+-- 			if filetype == b then
+-- 				vim.cmd.enew()
+-- 			end
+-- 		end
+-- 		vim.cmd("wincmd L | let w = min([90, &columns / 2]) | execute 'vertical resize ' . w")
+-- 	end,
+-- })
 
 -- Automatically create directory for file if it doesn't exist
 local group_auto_create_dir = augroup("group_auto_create_dir", { clear = true })
@@ -158,29 +172,60 @@ autocmd({ "BufEnter" }, {
 			"man",
 			"qf",
 			"startuptime",
+			"tags",
 			-- "vim", -- Is this needed?
 		}
 		local tab_close_types = {
 			"DiffviewFiles",
 			"DiffviewFileHistory",
 		}
+		-- local num_listed = #vim.tbl_filter(function(bufnr) return vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) end, vim.api.nvim_list_bufs())
 		for _, b in ipairs(simple_close_types) do
 			if filetype == b then
 				vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "", {
-					callback = function()
-						vim.api.nvim_command("close")
-					end,
+					callback = function() vim.api.nvim_command("bdelete") end,
 				})
 			end
 		end
 		for _, b in ipairs(tab_close_types) do
 			if filetype == b then
 				vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "", {
-					callback = function()
-						vim.api.nvim_command("tabclose")
-					end,
+					callback = function() vim.api.nvim_command("tabclose") end,
 				})
 			end
 		end
 	end,
 })
+
+-- resize splits if the terminal window is resized
+-- autocmd("VimResized", {
+-- 	command = "wincmd =",
+-- })
+
+-- This isn't working at the moment
+-- Automatically close Neovim if the only buffer open has a certain filetype
+-- local close_with_remaining_ft = augroup("close_with_remaining_ft", { clear = true })
+-- autocmd({ "BufDelete" }, {
+-- 	group = close_with_remaining_ft,
+-- 	callback = function(args)
+-- 		local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+-- 		print(#buffers)
+-- 		if #buffers ~= 1 then
+-- 			print("this occurred")
+-- 			return
+-- 		end
+--
+-- 		local bufnr = args.buf
+-- 		local filetype = vim.bo[bufnr].filetype
+-- 		local simple_close_types = {
+-- 			"snacks_picker_list",
+-- 		}
+-- 		-- local num_listed = #vim.tbl_filter(function(bufnr) return vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) end, vim.api.nvim_list_bufs())
+-- 		for _, b in ipairs(simple_close_types) do
+-- 			if filetype == b then
+-- 				print("matched ft")
+-- 				vim.api.nvim_command("q")
+-- 			end
+-- 		end
+-- 	end,
+-- })
